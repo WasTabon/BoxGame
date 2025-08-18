5660059cedc155fb75f8007914ef3a49575cd8a8
WasTabon <titanium.jen@gmail.com>
2025-08-18 21:14:47 +0300

    level

--- Assets/BoxGame/Scripts/ZonesController.cs
+++ Assets/BoxGame/Scripts/ZonesController.cs
@@ -1,9 +1,46 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class ZoneUIRule
+{
+    public string tag;
+    public UIKey key;
+}
 
 public class ZonesController : MonoBehaviour
 {
+    [Header("Правила зон -> UI")]
+    [SerializeField] private List<ZoneUIRule> zoneRules = new List<ZoneUIRule>();
+
+    private Dictionary<string, UIKey> rulesDictionary;
+
+    private void Awake()
+    {
+        rulesDictionary = new Dictionary<string, UIKey>();
+        foreach (var rule in zoneRules)
+        {
+            if (!rulesDictionary.ContainsKey(rule.tag))
+            {
+                rulesDictionary.Add(rule.tag, rule.key);
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (rulesDictionary.TryGetValue(other.tag, out UIKey key))
+        {
+            UIController.Instance.Show(key);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (rulesDictionary.TryGetValue(other.tag, out UIKey key))
+        {
+            UIController.Instance.Hide(key);
+        }
     }
 }
